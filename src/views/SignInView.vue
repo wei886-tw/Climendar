3d508a849c58932387506648b7a7bede9943a655
Wei <lunhowl@gmail.com>
2025-05-13 12:16:56 +0800

    更新安全設置並移除敏感檔案

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 border">
-        <div class="g-signin2 button btn btn-blue" data-onsuccess="onSignIn">用 Google 登入</div>
+        <div class="g-signin2 btn " data-onsuccess="onSignIn">用 Google 登入</div>
       </div>
     </div>
   </div>
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools()
   ],
-  base: '/Weatherndar/',
+  base: '/Climendar/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

d6b49dd5ba744768b74fbba58fb7e7e739acbdb9
Wei <lunhowl@gmail.com>
2025-05-13 16:19:06 +0800

    重新上傳新版 API

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="container-fluid">
     <div class="container pt-96">
-      <router-link to="/home" class="fs-24"><i class="bi bi-house "></i></router-link>
+      <router-link to="/home" class="fs-24"><i class="bi bi-house"></i></router-link>
       <router-link to="/calendar">Cal</router-link>
-      <router-link to=""></router-link>
-      <router-link to=""></router-link>
-      <router-link to=""></router-link>
+      <!-- Add more router-links with valid 'to' attributes as needed -->
     </div>
   </div>
 </template>
